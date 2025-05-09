55031ac29bd9806536d8e747d757aa0ae018beb4
Divyansh044 <divyanshbangwal@gmail.com>
2025-05-09 22:09:16 +0530

    Added requirements file

--- app.py
+++ app.py
@@ -186,5 +186,5 @@ def cleanup():
     shutil.rmtree(UPLOAD_FOLDER, ignore_errors=True)
 
 if __name__ == '__main__':
-   port = int(os.environ.get('PORT', 5000))
-   app.run(host='0.0.0.0', port=port)
+   port = int(os.environ.get('PORT', 8080))
+   app.run(host='0.0.0.0', port=port,debug=False)
